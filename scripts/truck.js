003a1c2c4b7df8ad0e38721642b3b8adc415ef2c
Jonas Barte <jonas@barte.se>
2018-04-21 22:58:55 +0200

    Added create order function

--- scripts/truck.js
+++ scripts/truck.js
@@ -1,1 +1,16 @@
 
+(function(window) {
+  'use strict';
+  var App = window.App || {};
+
+  function Truck(truckId, db) {
+    this.truckId = truckId;
+    this.db = db;
+  };
+  Truck.prototype.createOrder = function(order) {
+    console.log('Adding order for ' + order.emailAdress);
+    this.db.add(order.emailAdress, order);
+  }
+  App.Truck = Truck;
+  window.App = App;
+})(window);
